1fba8a0967ea2394dc425ff671f0a4fa08d6cd52
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-09-19 22:52:48 +0900

    Reset on second click. New grid will appear on third click

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
     const gridContainer = document.createElement("div");
     const container = [];
     const gridBox = [];
+    let firstClick = true;
 
     body.appendChild(btn);
     body.appendChild(gridContainer);
@@ -13,29 +14,50 @@
 
     
     btn.textContent = "Create a new drawing pad";  
-    btn.addEventListener('click', function () {
-        const userInput = prompt
-        ("Enter the number of squares (max 100) per side for the new grid.");
+    
+        btn.addEventListener('click', function () {
+            if (firstClick) {
+                const userInput = prompt
+                ("Enter the number of squares (max 100) per side for the new grid.");
 
+                if (isNaN(userInput)) {
+                    alert("Invalid input. Please enter a mumber.")
+                    }
 
-            for (let i = 0; i < userInput; i++) {
-                container[i] = document.createElement("div");
-                gridContainer.appendChild(container[i]);
-                container[i].className ="containers";
-        
-                for (let j = 0; j < userInput; j++) {
-                    gridBox[j] = document.createElement("div");
-                    container[i].appendChild(gridBox[j]);
-                    gridBox[j].className ="gridBoxes";
+                else if (userInput > 101) {
+                    alert("The number should not be bigger than 100.")
+                    }
 
-                    gridBox[j].addEventListener('mouseover', function (){
-                        gridBox[j].classList.add("black-background");
-                        });
-                    
+                else {
+                    for (let i = 0; i < userInput; i++) {
+                        container[i] = document.createElement("div");
+                        gridContainer.appendChild(container[i]);
+                        container[i].className ="containers";
+                
+                        for (let j = 0; j < userInput; j++) {
+                            gridBox[j] = document.createElement("div");
+
+                            gridBox[j].addEventListener('mouseover', function (){
+                            gridBox[j].classList.add("black-background"); 
+                            });
+                            
+                            gridBox[j].className ="gridBoxes"; 
+                            container[i].appendChild(gridBox[j]);
+                            }
+                        } 
+                    } 
+            
+                firstClick = false;   
+                } 
+
+            else {
+                gridContainer.innerHTML = '';
+                firstClick = true; 
                 }
-            }
-         
-        
-});
+            
+            });
+    
+
+   
 
     
